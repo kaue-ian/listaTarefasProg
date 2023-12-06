cf96bc0fc5886354406787d7b61d62383042baa2
kaue-ian <131874075+kaue-ian@users.noreply.github.com>
2023-12-06 03:00:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+/*Linkando os IDs do index.html nas variaveis*/
+
+const tarefaDeEntrada = document.getElementById("tarefa");
+const btn = document.getElementById("adicionar");
+const lista = document.getElementById("tarefas");
+const finalizadas = document.getElementById("finalizadas")
+btn.addEventListener("click", addTarefa);
+tarefaDeEntrada.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        addTarefa();
+    }
+});
+
+/*Adicionar nova tarefa a lista*/
+
+function addTarefa() {
+    const textTask = tarefaDeEntrada.value;
+    if (textTask.trim() !== "") {
+        const newTask = document.createElement("li");
+        newTask.innerHTML = `
+          ${textTask} <button class="excluir">\u00D7</button> <button class="finalizar">\&#10004</button>
+      `;
+        lista.appendChild(newTask);
+        tarefaInput.value = "";
+
+    }
+}
+
+/*Validações da tecla Enter*/
+
+lista.addEventListener("click", function (e) {
+    if (e.target.classList.contains("excluir")) {
+        e.target.parentElement.remove();
+        
+    }
+    if (e.target.classList.contains("finalizar")) {
+        const tarefa = e.target.parentElement;
+        const textTask = tarefa.innerText.split(" ")[0];
+        tarefa.innerHTML = `${textTask}<button class="excluir">\u00D7</button>`;
+        tarefa.style.color = 'green';
+        finalizadas.appendChild(tarefa);
+    }
+});
+
+/*validação tecla Enter para excluir*/
+
+finalizadas.addEventListener("click", function (e) {
+    if (e.target.classList.contains("excluir")) {
+        e.target.parentElement.remove()
+    }
+});
